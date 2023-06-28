ec0feba6d1f7ba687f0500461140601d45ede2d0
Willi <pipripheral@gmail.com>
2023-06-28 21:54:52 +0700

    test if change work

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -1,13 +1,12 @@
 package app
 
 import (
-	"fmt"
 	"github.com/piperipheral/Reloader-Custom/internal/pkg/cmd"
 )
 
 // Run runs the command
 func Run() error {
-	fmt.Print("hello")
+	print("hello")
 	cmda := cmd.NewReloaderCommand()
 	return cmda.Execute()
 }
