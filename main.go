723278e779458c849abfd44e484b59eaea65b877
Willi <pipripheral@gmail.com>
2023-06-28 21:59:07 +0700

    test if change work

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"github.com/piperipheral/Reloader-Custom/internal/pkg/app"
-	"os"
 )
 
 func main() {
 	print("asdasasfasfasdasd")
 	if err := app.Run(); err != nil {
-		os.Exit(1)
+		//os.Exit(1)
 	}
-	os.Exit(0)
+	//os.Exit(0)
 }
